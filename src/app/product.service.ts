021e2eb74f949e106e833d77cdbeeeb9be074d86
Brian Davidson <brian_davidson@newyorklife.com>
2019-03-05 09:45:56 -0500

    Module 3 product service undo
    Remove bad code.

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import 'rxjs/operators/map';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ProductService {
@@ -9,17 +9,11 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  // getAlbum(id: number) {
-  //   return this._http.get(this._albumUrl)
-  //   .pipe(map(response => {
-  //     return response.json();
-  //   }));
-  // }
-
-    getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map(response => {
-      response.json();
-    });
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl)
+    .pipe(map(response => {
+      return response.json();
+    }));
   }
 
 }
